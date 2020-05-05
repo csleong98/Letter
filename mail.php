877c9af620dd11ead7be11cf32d63089bae7d61b
csleong98 <macintoshleong@gmail.com>
2020-05-05 19:13:37 +0800

    added php

--- mail.php
+++ mail.php
@@ -1,6 +1,29 @@
-<html>
-    <body>
-       Your email address is <?php echo $_POST["email"]; ?>
-       <?php echo "done" ?>
-    </body>
-</html>
+<?php 
+
+if(!isset($_POST['email'])) {
+    died("Error");
+} 
+    $email_to = "macintoshleong@gmail.com";
+    $email_subject = "A Friendly Postcard";
+    
+    function died($error) {
+        // error code goes here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error. "<br></br>";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+    
+    $email_from = "spaceman@gmail.com";
+    $email = $_POST['email'];
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+    
+    $email_message = "This is the email".clean_string($email)."";
+    
+    // create email headers
+    $headers = 'From: '.$email_from."\r\n".
+    'Reply-To: '.$email_from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
