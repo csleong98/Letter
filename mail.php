4c3cfd7f1c3bc3952316dca51d0ce04179807431
csleong98 <macintoshleong@gmail.com>
2020-05-06 19:47:25 +0800

    updated php file

--- mail.php
+++ mail.php
@@ -1,18 +1,17 @@
 <?php
-   $to = $_POST['email'];
-   $subject = "This is subject";
+if (isset($_POST['submit'])) {
+    $to = $_POST['email'];
+    $from = "spaceman@hotmail.my";
+    $firstname = "Chee Seng";
+    $lastname = "Leong";
+    $subject = "Testing";
+    $subject2 = "Confirm this";
+    $message = $firstname . " " . $lastname . " " . "wrote the following:" . "\n\n" . "Hi there Chee Seng";
 
-   $message = "<b>This is HTML message.</b>";
-   $message .= "<h1>This is headline.</h1>";
-
-   $header = "From:mymailid@gmail.com \r\n";
-   $retval = mail($to,$subject,$message,$header);
-   if(isset($retval))//change
-   {
-       echo "Message sent successfully...";
-   }
-   else
-   {
-    echo "Message could not be sent...";
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    header('Location: index.html');
 }
 ?> 
+ 
