de29b58e767f4ae6e65f2aa1590dca42d183c40b
csleong98 <macintoshleong@gmail.com>
2020-05-07 21:14:19 +0800

    fixed mail.php, exploring download.js

--- js/download.js
+++ js/download.js
@@ -10,5 +10,26 @@ download.addEventListener("click", function () {
         a.download = "postcard";
         a.click();
         document.body.removeChild(a);
+
+        // const img = document.createElement('img');
+        // document.body.appendChild(img);
+        // img.src = canvas.toDataURL("image/png");
+        // img.setAttribute('method', 'post')
+        // img.setAttribute('name', 'image');
+        // img.setAttribute('style', 'display: block');
     });
+})
+
+var sendEmail = document.getElementById('send-letter');
+
+sendEmail.addEventListener("click", function() {
+    Email.send({
+        SecureToken: "12a2b8ae-9ddb-4187-8b0b-d5b6faecb1c3",
+        To : 'macintoshleong@gmail.com',
+        From : "cheeseng@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      console.log('successful')
+    );
 })
--- mail.php
+++ mail.php
@@ -1,59 +1,42 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
-/* Exception class. */
-require 'vendor/phpmailer/phpmailer/src/Exception.php';
-
-/* The main PHPMailer class. */
-require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-
-/* SMTP class, needed if you want to use SMTP. */
-require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 // Load Composer's autoloader
 require 'vendor/autoload.php';
 
+$mail = new PHPMailer(TRUE);
 
+//Server settings
+$mail->isSMTP();                  // Enable verbose debug output
+                                            // Send using SMTP
+$mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+$mail->Username   = 'macintoshleong@gmail.com';                     // SMTP username
+$mail->Password   = 'InfinityWorld2020';                               // SMTP password
+$mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
+//Recipients
+$mail->setFrom($_POST['email'], 'Mailer');
+$mail->addAddress('csleong@alphaus.cloud', 'Csleong');     // Add a recipient   // Name is optional
+$mail->addReplyTo('csleong@alphaus.cloud', 'Information');
 
-if (isset($_POST['submit'])) {
+// Attachments
 
-    try {
-        //Server settings
-        $email = new PHPMailer(TRUE);
-        $email->isSMTP();                  // Enable verbose debug output
-                                                   // Send using SMTP
-        $email->Host       = 'tls://smtp.gmail.com';                    // Set the SMTP server to send through
-        $email->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $email->Username   = 'macintoshleong@gmail.com';                     // SMTP username
-        $email->Password   = 'InfinityWorld2020';                               // SMTP password
-        $email->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $email->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-    
-        //Recipients
-        $email->setFrom($_POST['email'], 'Mailer');
-        $email->addAddress('csleong@alphaus.cloud', 'Csleong');     // Add a recipient   // Name is optional
-        $email->addReplyTo('csleong@alphaus.cloud', 'Information');
-    
-        // Attachments
-    
-        // Content
-        $email->isHTML(true);                                  // Set email format to HTML
-        $email->Subject = 'Here is the subject';
-        $email->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $email->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    
-        $email->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$email->ErrorInfo}";
-    }
+// Content
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->Body    = $_POST['image'];
 
-}
-
-else {
-    echo "Not detected";
+if(!$mail->send()){
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}else{
+    echo 'Message has been sent';
 }
 ?> 
  
