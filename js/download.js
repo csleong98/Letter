e2c23042ea472cd38d9704b46a3382e40955ba13
csleong98 <macintoshleong@gmail.com>
2020-05-09 16:10:27 +0800

    removed composer and node.js

--- js/download.js
+++ js/download.js
@@ -19,37 +19,3 @@ download.addEventListener("click", function () {
         // img.setAttribute('style', 'display: block');
     });
 })
-
-var sendEmail = document.getElementById('send-letter');
-const sgMail = require('node_modules/@sendgrid/mail');
-
-sendEmail.addEventListener("click", function() {
-    // Email.send({
-    //     SecureToken: "12a2b8ae-9ddb-4187-8b0b-d5b6faecb1c3",
-    //     To : 'macintoshleong@gmail.com',
-    //     From : "cheeseng@gmail.com",
-    //     Subject : "This is the subject",
-    //     Body : "And this is the body"
-    // }).then(
-    //   console.log('successful')
-    // );
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: 'macintoshleong@gmail.com',
-      from: 'test@example.com',
-      subject: 'Sending with Twilio SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    //ES6
-    sgMail
-      .send(msg)
-      .then(() => {}, error => {
-        console.error(error);
-    
-        if (error.response) {
-          console.error(error.response.body)
-        }
-      });
-
-})
