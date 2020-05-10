48322af1a32cb1fc2e5a2788d9a356c14574ecd2
csleong98 <macintoshleong@gmail.com>
2020-05-10 13:08:32 +0800

    removed green border during download

--- js/main.js
+++ js/main.js
@@ -65,6 +65,7 @@ var download = document.querySelector("#download");
 var imgConverted = document.getElementById("imgConverted");
 
 download.addEventListener("click", function () {
+    
     html2canvas(document.getElementById("postcard")).then(canvas => {
         const a = document.createElement('a');
 
